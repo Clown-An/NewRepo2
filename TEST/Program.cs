ab7a8d852deb44f7ecd119378b940f37b7f608bd
андрей новохатский <andrey_2n@mail.ru>
2021-05-16 20:43:14 +0700

    создание класса для работы с матрицей

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -6,6 +6,61 @@ using System.Threading.Tasks;
 
 namespace TEST
 {
+    class MyMatrix
+    {
+        protected int[,] matrix;
+        protected int dim;
+
+        public MyMatrix(int dimension)
+        {
+            this.dim = dimension;
+            this.matrix = new int[dimension, dimension];
+
+            //заполняем случайными числами от 0 до 100
+            Random random = new Random();
+            int rand;
+
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    rand = random.Next(0, 100);
+                    this.matrix[i, j] = rand;
+                }
+            }
+        }
+
+        public void OutMatrix()
+        {
+            for (int i = 0; i < this.dim; i++)
+            {
+                for (int j = 0; j < this.dim; j++)
+                {
+                    Console.Write(this.matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int MinMatrix()
+        {
+            int min = this.matrix[0, 0];
+
+            for (int i = 0; i < this.dim; i++)
+            {
+                for (int j = 0; j < this.dim; j++)
+                {
+                    if (this.matrix[i, j] < min)
+                    {
+                        min = this.matrix[i, j];
+                    }
+                }
+            }
+
+            return min;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -16,74 +71,17 @@ namespace TEST
             int r = Convert.ToInt16(Console.ReadLine());
 
             //создаём матрицу
-            int[,] Matrix = CreateMatrix(r);
-            
+            MyMatrix matrix = new MyMatrix(r);
+
             // выводим матрицу
-            OutMatrix(Matrix);
+            matrix.OutMatrix();
             Console.WriteLine("------------------");
 
             // поиск минимального элемента
-            Console.WriteLine(MinMatrix(Matrix));
+            Console.WriteLine(matrix.MinMatrix());
             
             Console.ReadKey();
         }
-
-
-        public static int MinMatrix(int[,] matrix)
-        {
-            int n = (int)Math.Sqrt(matrix.Length);
-            int min = matrix[0, 0];
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (matrix[i, j] < min)
-                    {
-                        min = matrix[i, j];
-                    }
-                }
-            }
-
-            return min;
-        }
-
-
-        public static void OutMatrix(int[,] matrix)
-        {
-            int n = (int)Math.Sqrt(matrix.Length);
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(matrix[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-        }
-
-
-        public static int[,] CreateMatrix(int n)
-        {
-            int[,] Matrix = new int[n, n];
-
-            //заполняем случайными числами от 0 до 100
-            Random random = new Random();
-            int rand;
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    rand = random.Next(0, 100);
-                    Matrix[i, j] = rand;
-                }
-            }
-
-            return Matrix;
-        }
-
     }
 }
 
